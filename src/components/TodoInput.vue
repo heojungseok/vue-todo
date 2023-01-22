231fc974c1ce0bcdcac73c5defe667352abdb433
heojungseok <tjrwjdgj@gmail.com>
2023-01-23 02:19:09 +0900

    feat: Create vue components
    header, input , list, footer

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Input</h1>
+    <input type="text" placeholder="input here !!!!">
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
